ecb00d75a86703c7d6599f228856632479cb82aa
Mo9tafa41 <mustafa.abazer.5o1101@gmail.com>
2025-12-25 13:31:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,20 @@ function game() {
     
     let result = document.getElementById('message');
 
-    if (document.getElementById('userInput').value == "") {
-        result.innerText = "الرجاء إدخال رقم أولاً!";
-        result.style.color = "orange";
-        return;
-    }
+    
 
     if (user_input < random_number) {
-        result.innerText = "الرقم صغير جداً! حاول مرة أخرى.";
+        result.innerText = "Try a larger number 🥴";
         result.style.color = "red";
     } 
     else if (user_input > random_number) {
-        result.innerText = "الرقم كبير جداً! حاول مرة أخرى.";
+        result.innerText = "Try a smaller number 🥴";
         result.style.color = "red";
     } 
     else if (user_input == random_number){
-        result.innerText = "مبروك! الإجابة صحيحة 🎉";
+        result.innerText = "Congratulations, you have won!🎉😍";
         result.style.color = "green"; 
-        
-        return(2000);
     }
-   }
+}
+
+   
